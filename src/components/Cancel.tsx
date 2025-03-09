5655f6be35be7ce21a7d923d515bfc62c4478942
rubiclmao <rubicmicrosoftmc@gmail.com>
2025-03-09 16:21:34 +0300

    bug-fix-close-button

--- src/components/Cancel.tsx
+++ src/components/Cancel.tsx
@@ -5,7 +5,7 @@ import { X } from "lucide-react";
 
 const NextPrev = () => {
   return (
-    <div className="flex absolute h-screen w-screen justify-end items-start z-10 p-10">
+    <div className="flex absolute top-0 w-screen justify-end content-start z-1 p-10">
       <Link href={"/"}>
         <X className="p-4 border-2 border-text rounded-full" size={64} />
       </Link>
